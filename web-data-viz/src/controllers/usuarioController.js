ad2181aec07dc9984a57fbb9359eff5c0bc0bbfc
OtavioVMLemos <otaviovmlemos@gmail.com>
2026-07-07 21:08:01 -0300

    feat: atualizando variaveis

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -135,7 +135,6 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var jogador = req.body.jogadorServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -144,12 +143,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (jogador == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, jogador)
+        usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
